1e5d108867dee1b6e348576eee78969dd5f80b8e
Alyssa Yates <bosslys@icloud.com>
2019-11-01 22:24:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/PigLatinizer' do
+  post '/PigLatinize' do
     @PigLatinizer = PigLatinizer.new(params[:user_text])
 
     erb :results
